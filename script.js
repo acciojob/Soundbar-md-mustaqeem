c767a9271221ea6d74fbb4abfebe2a685fb0977b
Md Mustaqeem <mdmustaqeem820@gmail.com>
2026-05-25 22:52:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+const sounds = [
+  "applause",
+  "boom",
+  "gasp",
+  "tada",
+  "victory"
+];
+
+const buttons = document.querySelectorAll(".btn");
+
+function stopSongs() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound);
+
+    song.pause();
+    song.currentTime = 0;
+  });
+}
+
+// Create audio elements
+sounds.forEach(sound => {
+
+  const audio = document.createElement("audio");
+
+  audio.src = `sounds/${sound}.mp3`;
+  audio.id = sound;
+
+  document.body.appendChild(audio);
+});
+
+buttons.forEach(button => {
+
+  button.addEventListener("click", () => {
+
+    const text = button.innerText.toLowerCase();
+
+    if (text === "stop") {
+      stopSongs();
+    } else {
+
+      stopSongs();
+
+      document.getElementById(text).play();
+    }
+  });
+
+});
